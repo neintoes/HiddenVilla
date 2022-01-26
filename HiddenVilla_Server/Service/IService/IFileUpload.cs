df80b546660b2a0f38a4720956e7eef3b2a20107
Ant <ant.m.meyer@gmail.com>
2022-01-26 00:43:40 +0000

    Created IFileUpload Interface

---
+++ HiddenVilla_Server/Service/IService/IFileUpload.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Service.IService
+{
+    public interface IFileUpload
+    {
+        public Task<string> FileUpload(IBrowser browser);
+
+        public bool DeleteFile(string filePath);
+    }
+}
