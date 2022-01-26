201346bf2ae0641281af95e871bffd027bd0cd0c
Ant <ant.m.meyer@gmail.com>
2022-01-26 01:05:31 +0000

    fixed interface again

--- HiddenVilla_Server/Service/FileUpload.cs
+++ HiddenVilla_Server/Service/FileUpload.cs
@@ -1,18 +1,28 @@
 ﻿namespace HiddenVilla_Server.Service
 {
     using IService;
+    using Microsoft.AspNetCore.Components.Forms;
+    using Microsoft.AspNetCore.Hosting;
+    using System;
     using System.Threading.Tasks;
 
     public class FileUpload : IFileUpload
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         public bool DeleteFile(string filePath)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public Task<string> UploadFile(IBrowser browser)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
--- HiddenVilla_Server/Service/IService/IFileUpload.cs
+++ HiddenVilla_Server/Service/IService/IFileUpload.cs
@@ -4,8 +4,8 @@ namespace HiddenVilla_Server.Service.IService
 {
     public interface IFileUpload
     {
-        public Task<string> UploadFile(IBrowser browser);
-
         public bool DeleteFile(string filePath);
+
+        public Task<string> UploadFile(IBrowser browser);
     }
 }
